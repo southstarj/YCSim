b51f09969314b8bbfe898d5ddc99cbde2bda531c
YF Wang <yfwang09@gmail.com>
2014-10-09 17:20:01 -0700

    report form printing adding

--- main.py
+++ main.py
@@ -25,39 +25,56 @@ Hw = 471.6;   # inject water enthalpy: btu/lb
 J = 10.0;     # normalized injectivity: lb/cf.psi
 steam = prop.steamProp("saturated_steam.org");
 
-Sg = Sg0;
-p = p0;
-[rhow, rhog, drhow, drhog, Uw, Ug, drhoUw, drhoUg] = calcProp(steam, p);
-Rw0 = rhow*(1-Sg) + rhog*Sg;
-Re0 = (rhow*(1-Sg)*Uw + rhog*Sg*Ug);
-v = calcProp(steam, 595.03);
-print 'Uw(595.03psi) =',v[4]
+Hw = 262.09;
+Jvalues = [0.005, 0.007, 0.00875, 0.00880];
 
-for iter in range(30):
-#    print 'Iteration #', iter
-    Rw = (rhow*(1-Sg) + rhog*Sg) - Rw0 - J*(pi-p);
-    Re = (rhow*(1-Sg)*Uw + rhog*Sg*Ug) - Re0 - J*Hw*(pi-p);
-    RHS = -np.array([Rw, Re]).transpose();
-#    print 'RHS = ', RHS
-    a11 = rhog-rhow;
-    a21 = rhog*Ug-rhow*Uw;
-    a12 = (1-Sg)*drhow + Sg*drhog + J;
-    a22 = (1-Sg)*drhoUw + Sg*drhoUg + J*Hw;
-    comp = a22 - a21/a11*a12;
-    Rebar = Re - a21/a11*Rw;
-#    print a11, a21, a12, a22
-    A = np.array([[a11, a12], [a21, a22]]);
-#    print A
-    x = np.linalg.solve(A, RHS);
-    Sg = Sg + x[0];
-    p = p + x[1];
-    """
-    print '    dSg =', x[0]
-    print '    dp =', x[1]
-    print '    Compressibility =', comp
-    print '  Sg =', Sg
-    print '  p =', p
-    print '    modified Re =', Rebar
-    """
-    print iter, '\t', x[0],'\t', x[1],'\t', comp,'\t', Sg,'\t', p,'\t', Rebar
+for J in Jvalues:
+    print '\centerline{Table',Jvalues.index(J),'}'
+    print '\centerline{Water Injection into Saturated Steam}'
+    print '\vspace{20pt}'
+
+    print '\begin{tabular}{ l l }'
+    print 'Injection pressure: &', pi, 'psi \\'
+    print 'Cell pressure: &', p0, 'psi \\'
+    print 'Cell gas saturation: &', Sg0, '\\'
+    print 'Injection enthalpy: &', Hw, 'Btu/lb.(Saturated water at 60psi) \\'
+    print '\end{tabular}'
+    print '\vspace{20pt}'
+
+    print '\begin{tabular}{ c c c c c c c }'
+    print 'Itn# & $\delta S_g$ & $\delta p$ & $\hat{a}_{22}$ & $S_g$ & $p$ & $\hat{R}_e$'
+
+    Sg = Sg0;
+    p = p0;
     [rhow, rhog, drhow, drhog, Uw, Ug, drhoUw, drhoUg] = calcProp(steam, p);
+    Rw0 = rhow*(1-Sg) + rhog*Sg;
+    Re0 = (rhow*(1-Sg)*Uw + rhog*Sg*Ug);
+
+    for iter in range(30):
+        #    print 'Iteration #', iter
+        Rw = (rhow*(1-Sg) + rhog*Sg) - Rw0 - J*(pi-p);
+        Re = (rhow*(1-Sg)*Uw + rhog*Sg*Ug) - Re0 - J*Hw*(pi-p);
+        RHS = -np.array([Rw, Re]).transpose();
+        #    print 'RHS = ', RHS
+        a11 = rhog-rhow;
+        a21 = rhog*Ug-rhow*Uw;
+        a12 = (1-Sg)*drhow + Sg*drhog + J;
+        a22 = (1-Sg)*drhoUw + Sg*drhoUg + J*Hw;
+        comp = a22 - a21/a11*a12;
+        Rebar = Re - a21/a11*Rw;
+        #    print a11, a21, a12, a22
+        A = np.array([[a11, a12], [a21, a22]]);
+        #    print A
+        x = np.linalg.solve(A, RHS);
+        Sg = Sg + x[0];
+        p = p + x[1];
+        """
+        print '    dSg =', x[0]
+        print '    dp =', x[1]
+        print '    Compressibility =', comp
+        print '  Sg =', Sg
+        print '  p =', p
+        print '    modified Re =', Rebar
+        """
+        print iter, '\t', x[0],'\t', x[1],'\t', comp,'\t', Sg,'\t', p,'\t', Rebar
+        [rhow, rhog, drhow, drhog, Uw, Ug, drhoUw, drhoUg] = calcProp(steam, p);
